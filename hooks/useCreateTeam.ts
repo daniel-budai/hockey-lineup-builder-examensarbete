1fb0413341bc2e3444be79777f8a49057534bc87
daniel-budai <danielbudai@tutanota.com>
2025-03-14 21:15:15 +0100

    typo

--- hooks/useCreateTeam.ts
+++ hooks/useCreateTeam.ts
@@ -1,7 +1,7 @@
 import { toast } from "sonner";
 
 export function useCreateTeam() {
-  const handleCreateTeam = async (teamData:) => {
+  const handleCreateTeam = async (teamData) => {
     try {
       const response = await fetch("/api/teams", {
         method: "POST",
